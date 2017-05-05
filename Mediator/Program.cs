683672099f40ba2b48c1a53ce8664a4a88eaebed
FeDLviv <fed.lviv@gmail.com>
2017-05-05 11:22:01 +0300

    Add Mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+/*
+ПАТЕРН ПОВЕДІНКИ
+ПОСЕРЕДНИК 
+Призначення:
+Визначає об’єкт, що інкапсулює взаємодію між множиною об’єктів.
+Медіатор покращує слабкозв’язність шляхом утримання об’єктів від прямих посилань один на одного, 
+а також дозволяє незалежно змінювати взаємодію.
+ */
+namespace Mediator
+{
+    //ТЕСТ
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dispatcher dispatcher = new Dispatcher();
+
+            MedicDepartment medic = new MedicDepartment(dispatcher);
+            FireDepartment fire = new FireDepartment(dispatcher);
+            Police police = new Police(dispatcher);
+
+            dispatcher.Medic = medic;
+            dispatcher.Fire = fire;
+            dispatcher.Police = police;
+
+            fire.SendMessage("Address: str.Shevchenka, 22");
+        }
+    }
+
+    //Посередник (Behavioral pattern Mediator)
+    public interface IMediator
+    {
+        void SendMessage(string msg, Сolleague service);
+    }
+
+    //Конкретний посередник (диспетчер)
+    public class Dispatcher : IMediator
+    {
+        public FireDepartment Fire { get; set; }
+        public MedicDepartment Medic { get; set; }
+        public Police Police { get; set; }
+
+        public void SendMessage(string msg, Сolleague service)
+        {
+            if (service is FireDepartment)
+            {
+                Medic.Attention(service.GetType().Name.ToString().ToUpper() + " " + msg);
+                Police.Attention(service.GetType().Name.ToString().ToUpper() + " " + msg);
+            }
+            else if (service is MedicDepartment && Medic.IsScope)
+            {
+                Police.Attention(service.GetType().Name.ToString().ToUpper() + " " + msg);
+            }
+            else if (service is Police && Police.IsMurder)
+            {
+                Medic.Attention(service.GetType().Name.ToString().ToUpper() + " " + msg);
+            }
+        }
+    }
+
+    //Абстрактний клас (колега)
+    public abstract class Сolleague
+    {
+        protected IMediator dispatcher;
+
+        public Сolleague(IMediator dispatcher)
+        {
+            this.dispatcher = dispatcher;
+        }
+
+        public void SendMessage(string message)
+        {
+            dispatcher.SendMessage(message, this);
+        }
+
+        public abstract void Attention(string msg);
+    }
+
+    //Конкретний колега (пожежники)
+    public class FireDepartment : Сolleague
+    {
+        public FireDepartment(IMediator dispatcher)
+            : base(dispatcher)
+        { }
+
+        public override void Attention(string msg)
+        {
+            Console.WriteLine("*** {0} ***", msg);
+            Console.WriteLine("Fire department - READY");
+        }
+    }
+
+    //Конкретний колега (швидка допомога)
+    public class MedicDepartment : Сolleague
+    {
+        public bool IsScope { get; set; }
+
+        public MedicDepartment(IMediator dispatcher)
+            : base(dispatcher)
+        { }
+
+        public override void Attention(string msg)
+        {
+            Console.WriteLine("*** {0} ***", msg);
+            Console.WriteLine("Medic department - READY");
+        }
+    }
+
+    //Конкретний колега (поліція)
+    public class Police : Сolleague
+    {
+        public bool IsMurder { get; set; }
+
+        public Police(IMediator dispatcher)
+            : base(dispatcher)
+        { }
+
+        public override void Attention(string msg)
+        {
+            Console.WriteLine("*** {0} ***", msg);
+            Console.WriteLine("Police - READY");
+        }
+    }
+}
---
+++ Mediator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mediator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LTE")]
+[assembly: AssemblyProduct("Mediator")]
+[assembly: AssemblyCopyright("Copyright © LTE 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aba9128e-a725-4054-955b-a668582c233d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
