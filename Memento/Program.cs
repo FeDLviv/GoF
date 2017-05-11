b0809ab8880c459df8adfd3059a01cea59ca8a1f
FeDLviv <fed.lviv@gmail.com>
2017-05-11 17:02:21 +0300

    Add Memento

---
+++ Memento/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        } 
+    }
+    
+    //Originator (game)
+    //Memento (save)
+    //Caretaker (state)
+}
